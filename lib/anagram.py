96e2f9ae37184785346bdaa6fdeb2f7e360379da
Lee Campbell <121746971+leevcampbell@users.noreply.github.com>
2023-04-07 14:39:12 -0400

    filled anagram class to pass tests

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
-# your code goes here!
+
+
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word.lower()
+        self.word_sorted = sorted(self.word)
+
+    def match(self, words):
+        return [word for word in words if self.is_anagram(word)]
+
+    def is_anagram(self, word):
+        word = word.lower()
+        return word != self.word and sorted(word) == self.word_sorted
